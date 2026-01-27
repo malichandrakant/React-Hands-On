ec3d4ffaefbc24a8cb1d19eada383fccc84ad32e
Mali Chandrakant <161549812+malichandrakant@users.noreply.github.com>
2026-01-27 17:58:20 +0530

    to show product using react

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import "./App.css"
+import Title from "./Title.jsx"
+import Subtitle from "./Subtitle.jsx"
+import ProductTab from "./ProductTab.jsx";
+
+function App() {
+  return (
+<ProductTab />
+  );
+}
+export default App
---
+++ Product.jsx
@@ -0,0 +1,11 @@
+
+import "./Product.css";
+function Product() {
+  return (
+    <div className="Product">
+      <h3>This is a product</h3>
+      <p>Product details go here.</p>
+    </div>
+  );
+}
+export default Product;
